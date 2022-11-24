b3d70a60140e54d1a48e6488b71f56948b20d03b
Shah Nawaj Fahim <shahnawasfahim@gmail.com>
2022-11-24 23:57:08 +0600

    login

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCnh-eD2R0jGn1c_m528v1d59DvjsfEFbc",
+    authDomain: "garage-21d76.firebaseapp.com",
+    projectId: "garage-21d76",
+    storageBucket: "garage-21d76.appspot.com",
+    messagingSenderId: "1051205239162",
+    appId: "1:1051205239162:web:73cb19f78f1852923f16c3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
